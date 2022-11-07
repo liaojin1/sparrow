e879d39e3d7c80a5a90d13da20c78f1d740ccd51
廖金 <liaojin@bytedance.com>
2022-11-07 14:55:52 +0800

    chore: init local env

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    // 这里增加一行，用于支持后面的测试环境
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+  },
+};
